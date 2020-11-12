246c1ed2cb6a4635f18b01af65c46089206f8046
Andhs <dmiok@hotmail.com>
2020-11-12 16:22:26 +0300

    Update extract_sentences.py

--- extract_sentences.py
+++ extract_sentences.py
@@ -11,6 +11,8 @@ def mined_sentences(alignments, scores, src_file, tgt_file, src_file_lines, tgt_
     mined_target_prep = open(tgt_file[:-9] + "_mined.txt", "w", encoding='utf-8-sig')
     mined_source = open(src_file[:-9] + "_mined_lines.txt", "w", encoding='utf-8-sig')
     mined_target = open(tgt_file[:-9] + "_mined_lines.txt", "w", encoding='utf-8-sig')
+    accepted = open(src_file[:-9] + "_accepted.txt", "w", encoding='utf-8-sig')
+    rejected = open(src_file[:-9] + "_rejected.txt", "w", encoding='utf-8-sig')
 
 
 
@@ -23,6 +25,7 @@ def mined_sentences(alignments, scores, src_file, tgt_file, src_file_lines, tgt_
                 # А нормальные строки записываем сначала в список, чтобы потом записать элементы через разделитель
                 line_source.append(source_lines[alignments[i][0][j]].strip("\n"))
             mined_source.write("   ".join(line_source))
+            accepted.write("   ".join(line_source))
             if i < len(alignments)-1:
                 mined_source_prep.write("\n")
                 mined_source.write("\n")
@@ -31,11 +34,28 @@ def mined_sentences(alignments, scores, src_file, tgt_file, src_file_lines, tgt_
                 # А нормальные строки записываем сначала в список, чтобы потом записать элементы через разделитель
                 line_target.append(target_lines[alignments[i][1][k]].strip("\n"))
             mined_target.write("   ".join(line_target))
+            accepted.write("\t" + "   ".join(line_target) + "\t" + scores[i])
             if i < len(alignments)-1:
                 mined_target_prep.write("\n")
                 mined_target.write("\n")
+                accepted.write("\n")
+        else:
+            line_source = []
+            line_target = []
+            for j in range(len(alignments[i][0])):
+                # А нормальные строки записываем сначала в список, чтобы потом записать элементы через разделитель
+                line_source.append(source_lines[alignments[i][0][j]].strip("\n"))
+            rejected.write("   ".join(line_source))
+            for k in range(len(alignments[i][1])):
+                # А нормальные строки записываем сначала в список, чтобы потом записать элементы через разделитель
+                line_target.append(target_lines[alignments[i][1][k]].strip("\n"))
+            rejected.write("\t" + "   ".join(line_target) + "\t" + scores[i])
+            if i < len(alignments)-1:
+                rejected.write("\n")
 
     mined_source_prep.close()
     mined_target_prep.close()
     mined_source.close()
     mined_target.close()
+    accepted.close()
+    rejected.close()
